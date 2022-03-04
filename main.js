02a0c2836139d4f37774d8cd24fcb2bf69ce45fd
bao2206 <baopro2206@gmail.com>
2022-03-04 22:18:59 +0700

    4/3

---
+++ main.js
@@ -0,0 +1,68 @@
+console.log("Ni");
+
+console.log("Helloooo");    
+
+// alert("Welcome to");
+// variable
+// = là phép gán
+// undefined: Tạo ra biến mà không có gtri
+var username = 'Bao';
+// console.log(username);
+var number_Student = 30;
+var tong = 100;
+
+
+var isLogin = true;
+var address = null;
+
+var n1 = 10;
+var n2 = "5";
+var s = n1+n2;
+console.log(s); //Tinh tong
+var hieu= n1-n2;
+console.log(hieu); //Tinh hieu
+var kq_Hieu = "Hieu la: " +hieu;
+console.log(kq_Hieu); //Tinh hieu
+
+var nhan = n1*n2;
+var kq_Nhan ="Nhan la: " +nhan;
+console.log(kq_Nhan);
+var chia = n1 / n2;
+var kq_Chia = "Chia la: "+ chia;
+console.log(kq_Chia);
+
+var chiaLayDu = 4 % 3;
+var kq_ChiaLayDu = "Phan du la: " +chiaLayDu;
+console.log(kq_ChiaLayDu);
+
+var count = 0;
+// var new_Count = ++count;
+
+var new_Count = count++;
+console.log(new_Count);
+
+const PI = 3.14; //
+
+var edge1 = 3;
+var edge2 = 4;
+var edge3 ;
+edge3  = Math.sqrt(Math.pow(edge1,2) + Math.pow(edge2,2));
+console.log(edge3);
+
+var n = 345;
+var unit;
+var ten;
+var hundred;
+var sum;
+
+hundred = Math.floor(n / 100);
+ten = Math.floor(n%100/10);
+unit = Math.floor(n%10);
+
+sum = hundred + ten + unit;
+console.log(sum);
+
+
+// Đầu vào
+// xử lý 
+// đầu ra
